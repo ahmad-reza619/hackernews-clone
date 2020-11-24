14ccbbf4ad96b2bfdf4bbe1e4a04581873a0d843
Reza <hungry.dev39@gmail.com>
2020-11-24 23:09:53 +0700

    fix issues on fast refresh unnamed functions this fix is caused by an anonymous function in graphql client that make fast refresh cannot preserve local component state

--- utils/graphql-client.js
+++ utils/graphql-client.js
@@ -2,7 +2,7 @@ import { GraphQLClient } from 'graphql-request';
 
 const endpoint = 'https://graphql.fauna.com/graphql';
 
-export default (token) => {
+export default function graphqlClient(token) {
   const secret = token || process.env.NEXT_PUBLIC_FAUNA_SECRET;
   return new GraphQLClient(
     endpoint,
