6962340229c2da6f98c23af956b5aecba32653e3
Reza <hungry.dev39@gmail.com>
2020-12-05 18:53:02 +0700

    make login view

--- pages/login.js
+++ pages/login.js
@@ -1,14 +1,52 @@
+import { useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import Layout from '../components/Layout';
 
 export default function Login() {
+  const router = useRouter();
+  const [errorMessage, setErrorMessage] = useState('');
+
+  async function onSubmit(evt) {
+    evt.preventDefault();
+    setErrorMessage('');
+
+    const formdata = new FormData(evt.currentTarget);
+    const plainFormData = Object.fromEntries(formdata.entries());
+    const formDataAsJsonString = JSON.stringify(plainFormData);
+
+    try {
+      const res = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: formDataAsJsonString,
+      });
+
+      if (res.ok) {
+        router.push('/');
+      } else {
+        throw new Error(await res.text());
+      }
+    } catch (error) {
+      console.error(error);
+      setErrorMessage(error.message);
+    }
+  }
+
   return (
     <Layout>
-      <form>
+      <form onSubmit={onSubmit}>
+        {
+          errorMessage &&
+          <div>{errorMessage}</div>
+        }
         <div className="p-1 m-2">
           <input
-            type="text"
+            type="email"
+            name="email"
             placeholder="email"
             className="border"
           />
@@ -16,6 +54,7 @@ export default function Login() {
         <div className="p-1 m-2">
           <input
             type="password"
+            name="password"
             placeholder="password"
             className="border"
           />
